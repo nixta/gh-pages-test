d567293da7f29093ee2404e713f927031d374dd3
Nicholas Furness <nfurness@nixta.com>
2014-01-15 20:10:34 -0500

    testing

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    dest: '2013/',
+    exclusions: ['build','node_modules','2013','2014'],
+    copy: {
+      main: {
+        src: '**/*',
+        dest: 'build/<%= dest %>',
+        filter: function(filepath) {
+          var filepathParts = filepath.split(require('path').sep);
+          var shouldExclude = (filepathParts.length === 1 && grunt.file.isFile(filepath)) ||
+                              (grunt.config('exclusions').indexOf(filepathParts[0]) !== -1);
+          if (shouldExclude) {
+            console.log("Skipping " + filepath + " " + grunt.config('exclusions') + " " + filepath.split(require('path').sep)[0]);
+          }
+          return !shouldExclude;
+        }
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: 'build',
+        push: false
+      },
+      src: '**/*'
+    }
+  });
+
+  // Load the plugin that provides the "copy" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy', 'gh-pages']);
+};
